468bea7e21daa1cb45524f1e059e0812d2a3b0b8
Matt Rose <mattroseinbox@gmail.com>
2021-08-23 02:30:08 +0430

    feat: add SearchUser service

---
+++ graph/resolver/search.resolvers.go
@@ -0,0 +1,14 @@
+package graph
+
+// This file will be automatically regenerated based on the schema, any resolver implementations
+// will be copied through when generating and any unknown code will be moved to the end.
+
+import (
+	"context"
+
+	"github.com/favecode/note-core/graph/model"
+)
+
+func (r *queryResolver) Search(ctx context.Context, expression string) (*model.Search, error) {
+	return r.Service.Search(ctx, expression)
+}
