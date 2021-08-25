a1aaf03d82695563d8857d7ed4ed72bd9503deae
Mahdi <mahdineer@pm.me>
2021-08-26 01:07:26 +0430

    feat: implement AcceptUser service

--- service/follower_service.go
+++ service/follower_service.go
@@ -15,6 +15,10 @@ func (s *Service) FollowUser(ctx context.Context, userID string) (*model.Followe
 		return nil, errors.New(err.Error())
 	}
 
+	if user.ID == userID {
+		return nil, errors.New("can not follow yourself")
+	}
+
 	followingUser, _ := s.User.GetUserByID(userID)
 
 	follower, _ := s.Follower.GetFollowerByUserIdAndFollowerId(userID, user.ID)
@@ -46,6 +50,10 @@ func (s *Service) UnfollowUser(ctx context.Context, userID string) (*model.Follo
 		return nil, errors.New(err.Error())
 	}
 
+	if user.ID == userID {
+		return nil, errors.New("can not unfollow yourself")
+	}
+
 	follower, _ := s.Follower.GetFollowerByUserIdAndFollowerId(userID, user.ID)
 
 	if len(follower.ID) < 1 {
@@ -55,4 +63,29 @@ func (s *Service) UnfollowUser(ctx context.Context, userID string) (*model.Follo
 	deletedFollower, _ := s.Follower.DeleteFollower(follower.ID)
 
 	return deletedFollower, nil
+}
+
+func (s *Service) AcceptUser(ctx context.Context, userID string) (*model.Follower, error) {
+	user, err := middleware.GetCurrentUserFromCTX(ctx)
+
+	if err != nil {
+		return nil, errors.New(err.Error())
+	}
+
+	follower, _ := s.Follower.GetFollowerByUserIdAndFollowerId(user.ID, userID)
+
+	if len(follower.ID) < 1 {
+		return nil, errors.New("follower not found")
+	}
+
+	if *follower.Status == 1 {
+		return follower, nil
+	}
+
+	status := 1
+	follower.Status = &status
+
+	updatedFollower, _ := s.Follower.UpdateFollower(follower)
+
+	return updatedFollower, nil
 }
