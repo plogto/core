0decdfe59db3cc885625899641e8cec77288c224
Matt Rose <mattroseinbox@gmail.com>
2021-09-12 04:13:16 +0430

    feat: save tags for AddPost service

---
+++ util/unique.go
@@ -0,0 +1,13 @@
+package util
+
+func UniqueSliceElement(strSlice []string) []string {
+	keys := make(map[string]bool)
+	list := []string{}
+	for _, entry := range strSlice {
+		if _, value := keys[entry]; !value {
+			keys[entry] = true
+			list = append(list, entry)
+		}
+	}
+	return list
+}
