812f868264954fb6ca33f7f3ed6e8ebd07a32177
Matt Rose <mattroseinbox@gmail.com>
2021-09-11 21:56:20 +0430

    feat: add tag and post_tag table

--- database/migrations/1_poster_postgres.down.sql
+++ database/migrations/1_poster_postgres.down.sql
@@ -1,14 +1,17 @@
+-- Foreign keys
 ALTER TABLE "password" DROP CONSTRAINT IF EXISTS "password_fk0";
-
 ALTER TABLE "connection" DROP CONSTRAINT IF EXISTS "connection_fk0";
 ALTER TABLE "connection" DROP CONSTRAINT IF EXISTS "connection_fk1";
+ALTER TABLE "post_tag" DROP CONSTRAINT IF EXISTS "post_tag_fk0";
+ALTER TABLE "post_tag" DROP CONSTRAINT IF EXISTS "post_tag_fk1";
 
+-- Tables
 DROP TABLE IF EXISTS "user";
-
 DROP TABLE IF EXISTS "password";
-
 DROP TABLE IF EXISTS "post";
-
 DROP TABLE IF EXISTS "connection";
+DROP TABLE IF EXISTS "tag";
+DROP TABLE IF EXISTS "post_tag";
 
+-- Types
 DROP TYPE IF EXISTS "user_roles";
