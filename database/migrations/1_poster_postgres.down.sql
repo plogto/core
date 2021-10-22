f770fe2f2c782f11a0a68e8bc021548866bfb477
favecode <favecode@gmail.com>
2021-10-22 19:53:05 +0330

    feat: add post_comment_like table

--- database/migrations/1_poster_postgres.down.sql
+++ database/migrations/1_poster_postgres.down.sql
@@ -11,6 +11,8 @@ ALTER TABLE "post_save" DROP CONSTRAINT IF EXISTS "post_save_fk1";
 ALTER TABLE "post_comment" DROP CONSTRAINT IF EXISTS "post_comment_fk0";
 ALTER TABLE "post_comment" DROP CONSTRAINT IF EXISTS "post_comment_fk1";
 ALTER TABLE "post_comment" DROP CONSTRAINT IF EXISTS "post_comment_fk2";
+ALTER TABLE "post_comment_like" DROP CONSTRAINT IF EXISTS "post_comment_like_fk0";
+ALTER TABLE "post_comment_like" DROP CONSTRAINT IF EXISTS "post_comment_like_fk1";
 
 -- Tables
 DROP TABLE IF EXISTS "user";
@@ -22,6 +24,7 @@ DROP TABLE IF EXISTS "post_tag";
 DROP TABLE IF EXISTS "post_like";
 DROP TABLE IF EXISTS "post_save";
 DROP TABLE IF EXISTS "post_comment";
+DROP TABLE IF EXISTS "post_comment_like";
 
 -- Types
 DROP TYPE IF EXISTS "user_roles";
