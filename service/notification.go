dc4ae2d32fdcaac342e03dea9fea9b6969273ca0
Mahdi <favecode@gmail.com>
2021-11-10 16:32:11 +0330

    feat: implement GetNotification

--- service/notification.go
+++ service/notification.go
@@ -19,16 +19,15 @@ func (s *Service) GetNotification(ctx context.Context) (<-chan *model.Notificati
 		<-ctx.Done()
 		s.mu.Lock()
 		s.OnlineUser.DeleteOnlineUserBySocketId(onlineUserContext.SocketID)
+		delete(s.Notifications, onlineUserContext.SocketID)
 		s.mu.Unlock()
 	}()
-	// r.mu.Lock()
+
+	s.mu.Lock()
 	// // Keep a reference of the channel so that we can push changes into it when new messages are posted.
-	// r.ChatObservers[id] = msgs
-	// r.mu.Unlock()
-	// // This is optional, and this allows newly subscribed clients to get a list of all the messages that have been
-	// // posted so far. Upon subscribing the client will be pushed the messages once, further changes are handled
-	// // in the PostMessage mutation.
-	// r.ChatObservers[id] <- r.ChatMessages
+	notification := make(chan *model.Notification, 1)
+	s.Notifications[onlineUserContext.SocketID] = notification
+	s.mu.Unlock()
 
 	return nil, nil
 }
