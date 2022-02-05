6f52321fd1eae694632d2d4647aaf9dcfd275448
Matt Rose <mattroseinbox@gmail.com>
2022-02-06 01:46:29 +0300

    refactor: remove comment tables and change structure of post table

--- database/migrations/1_plog_postgres.up.sql
+++ database/migrations/1_plog_postgres.up.sql
@@ -37,6 +37,8 @@ CREATE TABLE "password" (
 CREATE TABLE "post" (
 	"id" uuid NOT NULL DEFAULT uuid_generate_v4(),
 	"user_id" uuid NOT NULL,
+	"parent_id" uuid DEFAULT NULL,
+	"child_id" uuid DEFAULT NULL,
 	"content" TEXT NOT NULL,
 	"attachment" TEXT DEFAULT NULL,
 	"url" VARCHAR(20) NOT NULL,
@@ -109,32 +111,6 @@ CREATE TABLE "post_save" (
   OIDS=FALSE
 );
 
-CREATE TABLE "comment" (
-	"id" uuid NOT NULL DEFAULT uuid_generate_v4(),
-	"parent_id" uuid DEFAULT NULL,
-	"user_id" uuid NOT NULL,
-	"post_id" uuid NOT NULL,
-	"content" TEXT NOT NULL,
-	"created_at" TIMESTAMP NOT NULL DEFAULT (NOW()),
-	"updated_at" TIMESTAMP NOT NULL DEFAULT (NOW()),
-	"deleted_at" TIMESTAMP,
- CONSTRAINT "comment_pk" PRIMARY KEY ("id")
-) WITH (
-  OIDS=FALSE
-);
-
-CREATE TABLE "comment_like" (
-	"id" uuid NOT NULL DEFAULT uuid_generate_v4(),
-	"user_id" uuid NOT NULL,
-	"comment_id" uuid NOT NULL,
-	"created_at" TIMESTAMP NOT NULL DEFAULT (NOW()),
-	"updated_at" TIMESTAMP NOT NULL DEFAULT (NOW()),
-	"deleted_at" TIMESTAMP,
- CONSTRAINT "comment_like_pk" PRIMARY KEY ("id")
-) WITH (
-  OIDS=FALSE
-);
-
 CREATE TABLE "online_user" (
 	"id" uuid NOT NULL DEFAULT uuid_generate_v4(),
 	"user_id" uuid NOT NULL,
@@ -195,8 +171,6 @@ CREATE TRIGGER update_tag BEFORE UPDATE ON "tag" FOR EACH ROW EXECUTE PROCEDURE 
 CREATE TRIGGER update_post_tag BEFORE UPDATE ON "post_tag" FOR EACH ROW EXECUTE PROCEDURE  trigger_set_updated_at();
 CREATE TRIGGER update_post_like BEFORE UPDATE ON "post_like" FOR EACH ROW EXECUTE PROCEDURE  trigger_set_updated_at();
 CREATE TRIGGER update_post_save BEFORE UPDATE ON "post_save" FOR EACH ROW EXECUTE PROCEDURE  trigger_set_updated_at();
-CREATE TRIGGER update_comment BEFORE UPDATE ON "comment" FOR EACH ROW EXECUTE PROCEDURE  trigger_set_updated_at();
-CREATE TRIGGER update_comment_like BEFORE UPDATE ON "comment_like" FOR EACH ROW EXECUTE PROCEDURE  trigger_set_updated_at();
 CREATE TRIGGER update_online_user BEFORE UPDATE ON "online_user" FOR EACH ROW EXECUTE PROCEDURE  trigger_set_updated_at();
 CREATE TRIGGER update_notification_type BEFORE UPDATE ON "notification_type" FOR EACH ROW EXECUTE PROCEDURE  trigger_set_updated_at();
 CREATE TRIGGER update_notification BEFORE UPDATE ON "notification" FOR EACH ROW EXECUTE PROCEDURE  trigger_set_updated_at();
@@ -204,6 +178,8 @@ CREATE TRIGGER update_notification BEFORE UPDATE ON "notification" FOR EACH ROW 
 -- Foreign keys
 ALTER TABLE "password" ADD CONSTRAINT "password_fk0" FOREIGN KEY ("user_id") REFERENCES "user"("id");
 ALTER TABLE "post" ADD CONSTRAINT "post_fk0" FOREIGN KEY ("user_id") REFERENCES "user"("id");
+ALTER TABLE "post" ADD CONSTRAINT "post_fk1" FOREIGN KEY ("parent_id") REFERENCES "post"("id");
+ALTER TABLE "post" ADD CONSTRAINT "post_fk2" FOREIGN KEY ("child_id") REFERENCES "post"("id");
 ALTER TABLE "connection" ADD CONSTRAINT "connection_fk1" FOREIGN KEY ("follower_id") REFERENCES "user"("id");
 ALTER TABLE "connection" ADD CONSTRAINT "connection_fk0" FOREIGN KEY ("following_id") REFERENCES "user"("id");
 ALTER TABLE "post_tag" ADD CONSTRAINT "post_tag_fk0" FOREIGN KEY ("tag_id") REFERENCES "tag"("id");
@@ -212,11 +188,6 @@ ALTER TABLE "post_like" ADD CONSTRAINT "post_like_fk0" FOREIGN KEY ("user_id") R
 ALTER TABLE "post_like" ADD CONSTRAINT "post_like_fk1" FOREIGN KEY ("post_id") REFERENCES "post"("id");
 ALTER TABLE "post_save" ADD CONSTRAINT "post_save_fk0" FOREIGN KEY ("user_id") REFERENCES "user"("id");
 ALTER TABLE "post_save" ADD CONSTRAINT "post_save_fk1" FOREIGN KEY ("post_id") REFERENCES "post"("id");
-ALTER TABLE "comment" ADD CONSTRAINT "comment_fk0" FOREIGN KEY ("parent_id") REFERENCES "comment"("id");
-ALTER TABLE "comment" ADD CONSTRAINT "comment_fk1" FOREIGN KEY ("user_id") REFERENCES "user"("id");
-ALTER TABLE "comment" ADD CONSTRAINT "comment_fk2" FOREIGN KEY ("post_id") REFERENCES "post"("id");
-ALTER TABLE "comment_like" ADD CONSTRAINT "comment_like_fk0" FOREIGN KEY ("user_id") REFERENCES "user"("id");
-ALTER TABLE "comment_like" ADD CONSTRAINT "comment_like_fk1" FOREIGN KEY ("comment_id") REFERENCES "comment"("id");
 ALTER TABLE "online_user" ADD CONSTRAINT "online_user_fk0" FOREIGN KEY ("user_id") REFERENCES "user"("id");
 ALTER TABLE "notification" ADD CONSTRAINT "notification_fk0" FOREIGN KEY ("notification_type_id") REFERENCES "notification_type"("id");
 ALTER TABLE "notification" ADD CONSTRAINT "notification_fk1" FOREIGN KEY ("sender_id") REFERENCES "user"("id");
