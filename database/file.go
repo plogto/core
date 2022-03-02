e28f29b465f4d1bea256aa65c80f83638af12f46
Mahdi <mahdineer@pm.me>
2022-03-03 01:44:25 +0300

    feat: implement SingleUploadFile service

---
+++ database/file.go
@@ -0,0 +1,31 @@
+package database
+
+import (
+	"fmt"
+
+	"github.com/go-pg/pg/v10"
+	"github.com/plogto/core/graph/model"
+)
+
+type File struct {
+	DB *pg.DB
+}
+
+func (f *File) GetFileByField(field string, value string) (*model.File, error) {
+	var file model.File
+	err := f.DB.Model(&file).Where(fmt.Sprintf("%v = ?", field), value).Where("deleted_at is ?", nil).First()
+	if len(file.ID) < 1 {
+		return nil, nil
+	}
+	return &file, err
+}
+
+func (f *File) GetFileByHash(hash string) (*model.File, error) {
+	return f.GetFileByField("hash", hash)
+}
+
+func (f *File) CreateFile(file *model.File) (*model.File, error) {
+	_, err := f.DB.Model(file).Returning("*").Insert()
+
+	return file, err
+}
