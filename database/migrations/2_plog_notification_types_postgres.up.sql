4586b37c7fb36231105f1195e5b9715e7bde3597
Matt Rose <mattroseinbox@gmail.com>
2022-07-18 23:35:15 +0300

    chore: remove database migrations

--- database/migrations/2_plog_notification_types_postgres.up.sql
+++
@@ -1,8 +0,0 @@
-TRUNCATE TABLE IF EXISTS "notification_type";
-INSERT INTO notification_type("name", "template")
-VALUES
-	('LIKE_POST','$$$___sender.username___$$$ liked your post.'),
-	('REPLY_POST','$$$___sender.username___$$$ replied to your post: $$$___post.content___$$$.'),
-	('LIKE_REPLY','$$$___sender.username___$$$ liked your reply.'),
-	('FOLLOW_USER','$$$___sender.username___$$$ started following you.'),
-	('ACCEPT_USER','$$$___sender.username___$$$ accepted your request.');
