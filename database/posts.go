abf39398f43e3102d7cdf9c5c7406785d39c97fd
Mahdi <mahdineer@pm.me>
2022-08-04 22:55:35 +0200

    perf: add hasNextPage to GetPostsByUserIDAndPagination

--- database/posts.go
+++ database/posts.go
@@ -54,11 +54,17 @@ func (p *Posts) GetPostsByUserIDAndPagination(userID string, parentID *string, l
 
 	endCursor := util.ConvertCreateAtToCursor(*edges[len(edges)-1].Node.CreatedAt)
 
+	hasNextPage := false
+	if totalCount > limit {
+		hasNextPage = true
+	}
+
 	return &model.Posts{
 		TotalCount: &totalCount,
 		Edges:      edges,
 		PageInfo: &model.PageInfo{
-			EndCursor: endCursor,
+			EndCursor:   endCursor,
+			HasNextPage: &hasNextPage,
 		},
 	}, err
 }
