77366d7c3e8b7f2437be647b33105b108c71b411
Mahdi <mahdineer@pm.me>
2022-08-08 23:49:52 +0200

    refactor: improve Liked services

--- service/liked_post_service.go
+++ service/liked_post_service.go
@@ -3,13 +3,15 @@ package service
 import (
 	"context"
 	"errors"
+	"fmt"
 
 	"github.com/plogto/core/constants"
 	"github.com/plogto/core/graph/model"
 	"github.com/plogto/core/middleware"
+	"github.com/plogto/core/util"
 )
 
-func (s *Service) LikePost(ctx context.Context, postID string) (*model.Post, error) {
+func (s *Service) LikePost(ctx context.Context, postID string) (*model.LikedPost, error) {
 	user, err := middleware.GetCurrentUserFromCTX(ctx)
 
 	if err != nil {
@@ -22,28 +24,18 @@ func (s *Service) LikePost(ctx context.Context, postID string) (*model.Post, err
 		return nil, errors.New("access denied")
 	}
 
-	postLike, _ := s.LikedPosts.GetPostLikeByUserIDAndPostID(user.ID, postID)
+	likedPost, _ := s.LikedPosts.GetLikedPostByUserIDAndPostID(user.ID, postID)
 
-	if postLike != nil {
-		s.LikedPosts.DeletePostLikeByID(postLike.ID)
-		s.RemoveNotification(CreateNotificationArgs{
-			Name:       constants.NOTIFICATION_LIKE_POST,
-			SenderID:   user.ID,
-			ReceiverID: post.UserID,
-			Url:        "/p/" + post.Url,
-			PostID:     &post.ID,
-		})
-	} else {
-		postLike := &model.LikedPost{
+	if util.IsEmpty(likedPost.ID) {
+		likedPost, err := s.LikedPosts.CreateLikedPost(&model.LikedPost{
 			UserID: user.ID,
 			PostID: postID,
-		}
+		})
 
-		s.LikedPosts.CreatePostLike(postLike)
+		fmt.Println(likedPost)
 
-		if len(postLike.ID) > 0 {
+		if !util.IsEmpty(likedPost.ID) {
 			var name string = constants.NOTIFICATION_LIKE_POST
-
 			if post.ParentID != nil {
 				name = constants.NOTIFICATION_LIKE_REPLY
 			}
@@ -56,9 +48,22 @@ func (s *Service) LikePost(ctx context.Context, postID string) (*model.Post, err
 				PostID:     &post.ID,
 			})
 		}
-	}
 
-	return post, nil
+		return likedPost, err
+
+	} else {
+		unlikedPost, err := s.LikedPosts.DeleteLikedPostByID(likedPost.ID)
+
+		s.RemoveNotification(CreateNotificationArgs{
+			Name:       constants.NOTIFICATION_LIKE_POST,
+			SenderID:   user.ID,
+			ReceiverID: post.UserID,
+			Url:        "/p/" + post.Url,
+			PostID:     &post.ID,
+		})
+
+		return unlikedPost, err
+	}
 }
 
 func (s *Service) GetLikedPostsByPostID(ctx context.Context, postID string) (*model.LikedPosts, error) {
@@ -86,7 +91,7 @@ func (s *Service) IsPostLiked(ctx context.Context, postID string) (*model.LikedP
 	if s.CheckUserAccess(user, followingUser) == bool(false) {
 		return nil, nil
 	} else {
-		isPostLiked, err := s.LikedPosts.GetPostLikeByUserIDAndPostID(user.ID, postID)
+		isPostLiked, err := s.LikedPosts.GetLikedPostByUserIDAndPostID(user.ID, postID)
 		if len(isPostLiked.ID) < 1 {
 			return nil, nil
 		}
