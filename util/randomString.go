50e67d66b4513233a2f6ba747dc9fe993d5bc3ba
Matt Rose <mattroseinbox@gmail.com>
2022-09-17 21:54:09 +0200

    refactor(randomString): add RandomHexString and RandomStringByLetters

--- util/randomString.go
+++ util/randomString.go
@@ -9,10 +9,30 @@ func init() {
 	rand.Seed(time.Now().UnixNano())
 }
 
-const letterBytes = "abcdefghijklmnopqrstuvwxyz1234567890"
+const defaultLetters = "abcdefghijklmnopqrstuvwxyz1234567890"
 
 func RandomString(n int) string {
+	return generateRandomString(n, nil)
+}
+
+func RandomHexString(n int) string {
+	return RandomStringByLetters(n, "abcdef0123456789")
+}
+
+func RandomStringByLetters(n int, letters string) string {
+	return generateRandomString(n, &letters)
+}
+
+func generateRandomString(n int, letters *string) string {
 	b := make([]byte, n)
+	var letterBytes string
+
+	if letters != nil {
+		letterBytes = *letters
+	} else {
+		letterBytes = defaultLetters
+	}
+
 	for i := range b {
 		b[i] = letterBytes[rand.Intn(len(letterBytes))]
 	}
