938f6eae98122e4c008235b18c2b2651aa0e5c6f
Matt Rose <mattroseinbox@gmail.com>
2022-10-27 00:16:00 +0200

    feat: add some fixtures

---
+++ fixtures/likedPost.go
@@ -0,0 +1,6 @@
+package fixtures
+
+import "github.com/plogto/core/graph/model"
+
+var EmptyLikedPost = &model.LikedPost{}
+var LikedPostWithID = &model.LikedPost{ID: "id"}
---
+++ fixtures/post.go
@@ -0,0 +1,6 @@
+package fixtures
+
+import "github.com/plogto/core/graph/model"
+
+var EmptyPost = &model.Post{}
+var PostWithID = &model.Post{ID: "id"}
