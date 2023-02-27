bb99eeb08111d1f1d5df6e52f6f8d874e4cdbd1c
Mahdi <mahdineer@pm.me>
2023-02-27 23:29:05 +0100

    refactor(PostAttachments): improve queries

--- database/post_attachments.go
+++ database/post_attachments.go
@@ -1,15 +1,21 @@
 package database
 
 import (
-	"github.com/go-pg/pg/v10"
-	"github.com/plogto/core/graph/model"
+	"context"
+
+	"github.com/plogto/core/db"
 )
 
 type PostAttachments struct {
-	DB *pg.DB
+	Queries *db.Queries
 }
 
-func (p *PostAttachments) CreatePostAttachment(postAttachment *model.PostAttachment) (*model.PostAttachment, error) {
-	_, err := p.DB.Model(postAttachment).Returning("*").Insert()
-	return postAttachment, err
+func (p *PostAttachments) CreatePostAttachment(ctx context.Context, arg db.CreatePostAttachmentParams) (*db.PostAttachment, error) {
+	postAttachment, err := p.Queries.CreatePostAttachment(ctx, arg)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return postAttachment, nil
 }
